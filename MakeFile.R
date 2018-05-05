76a792bf1ee587f7909acef95eaff717e07e8803
vnijs <radiant@rady.ucsd.edu>
2018-05-05 12:38:46 -0700

    pkgdown

--- MakeFile.R
+++ MakeFile.R
@@ -1,6 +1,7 @@
 library(knitr)
 library(dplyr)
 library(magrittr)
+library(glue)
 
 options(
   width = 250,
@@ -23,18 +24,53 @@ sapply(c("data","design","basics","model","multivariate"), knit_docs)
 
 ## setup articles for pkgdown
 setwd("~/gh/docs/")
+make_content <- function(files, app) {
+  for (file in files) {
+    # print(file)
+    article <- readLines(file.path(glue("{app}/app"), file))
+    figs <- ifelse(app == "data", "figures", glue("figures_{app}"))
+    article <- gsub(figs, glue("https://radiant-rstats.github.io/docs/{app}/{figs}/"), article)
+    file <- paste0("_", sub("\\.md$", ".Rmd", file))
+    cat(article, file = file.path(glue("../radiant.{app}/vignettes/pkgdown/{file}")), sep = "\n")
+  }
+}
+
 files <- c(
   "combine.Rmd", "explore.md", "manage.md", "pivotr.md",
   "report_r.md", "report_rmd.md", "state.md", "transform.Rmd",
   "view.Rmd", "visualize.md"
 )
 
-for (file in files) {
-  article <- readLines(file.path("data/app", file))
-  article <- gsub("figures/", "https://radiant-rstats.github.io/docs/data/figures/", article)
-  file <- paste0("_", sub("\\.md$", ".Rmd", file))
-  cat(article, file = file.path("../radiant.data/vignettes/pkgdown/", file), sep = "\n")
-}
+make_content(files, "data")
+
+files <- c(
+  "doe.md", "sample_size.Rmd", "sample_size_comp.Rmd", "sampling.md"
+)
+
+make_content(files, "design")
+
+files <- c(
+  "clt.md", "compare_means.md", "compare_props.md", "correlation.md",
+  "cross_tabs.md", "goodness.md", "prob_calc.Rmd", "single_mean.md",
+  "single_prop.md"
+)
+
+make_content(files, "basics")
+
+files <- c(
+  "crs.md", "crtree.md", "dtree.Rmd", "evalbin.md",
+  "evalreg.md", "logistic.Rmd", "nb.md", "nn.md",
+  "regress.Rmd", "simulater.Rmd"
+)
+
+make_content(files, "model")
+
+files <- c(
+  "conjoint.md", "full_factor.md", "hclus.md", "kclus.md",
+  "mds.md", "pre_factor.md", "prmap.md"
+)
+
+make_content(files, "multivariate")
 
 ## making the README.md file after clean-up
 setwd("~/gh/docs/")
