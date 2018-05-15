bd520dab156a0a9dbe2b6689d2469c0393835d3c
vnijs <radiant@rady.ucsd.edu>
2018-05-15 16:16:43 -0700

    update

--- MakeFile.R
+++ MakeFile.R
@@ -79,48 +79,61 @@ setwd("~/gh/docs/sub")
 knit("README_dev.Rmd")
 knit("tutorials_dev.Rmd")
 
-file.copy("README_dev.md",file.path("../../radiant.data/inst/app/tools/app/about.md"),overwrite = TRUE)
-file.copy("tutorials_dev.md",file.path("../../radiant.data/inst/app/tools/app/tutorials.md"),overwrite = TRUE)
+file.copy(
+  "README_dev.md",
+  file.path("../../radiant.data/inst/app/tools/app/about.md"),
+  overwrite = TRUE
+)
+
+file.copy(
+  "tutorials_dev.md",
+  file.path("../../radiant.data/inst/app/tools/app/tutorials.md"),
+  overwrite = TRUE
+)
 
 copy_docs <- function(app) {
   readLines("README_dev.md") %>% paste0(., collapse = "\n") %>%
-    sub("radiant.png",paste0(app,".png"), . ) %>%
-    sub("radiant-rstats/radiant)",paste0("radiant-rstats/",app,")"), ., fixed = TRUE) %>%
-    sub("badges/version/radiant)",paste0("badges/version/",app,")"), ., fixed = TRUE) %>%
-    sub("package=radiant)",paste0("package=",app,")"), ., fixed = TRUE) %>%
-    sub("radiant/issues",paste0(app,"/issues"), ., fixed = TRUE) %>%
-    cat(file = file.path("../..",app,"README.md"))
+    sub("radiant.png", paste0(app, ".png"), . ) %>%
+    sub("radiant-rstats/radiant)", paste0("radiant-rstats/", app,")"), ., fixed = TRUE) %>%
+    sub("badges/version/radiant)", paste0("badges/version/", app,")"), ., fixed = TRUE) %>%
+    sub("package=radiant)", paste0("package=",app,")"), ., fixed = TRUE) %>%
+    sub("radiant/issues", paste0(app,"/issues"), ., fixed = TRUE) %>%
+    cat(file = file.path("../..", app, "README.md"))
 }
 
-sapply(c("radiant","radiant.data","radiant.design","radiant.basics","radiant.model","radiant.multivariate"), copy_docs)
+sapply(
+  c("radiant","radiant.data","radiant.design",
+    "radiant.basics","radiant.model","radiant.multivariate"),
+  copy_docs
+)
 
 ## sync (R)md files to gh/radiant
 setwd("~/gh/docs")
 system('sh rsync_docs2app.sh')
 
 ## create documentation pdfs
-unlink('radiant.data.pdf')
-setwd("~")
-unlink('radiant.data.pdf')
-system("R CMD Rd2pdf gh/radiant.data --no-preview")
-system("rm -rf .Rd2pdf*")
-setwd("~/gh/docs/")
-file.copy("~/radiant.data.pdf","radiant.data.pdf",overwrite = TRUE)
-system("rm -rf .Rd2pdf*")
-
-create_manuals <- function(x) {
-  app <- paste0("radiant.",x)
-  man <- paste0(app,".pdf")
-  unlink(man)
-  setwd("~")
-  unlink(man)
-  system(paste0("R CMD Rd2pdf gh/",app," --no-preview"))
-  system("rm -rf .Rd2pdf*")
-  setwd("~/gh/docs/")
-  file.copy(paste0("~/",man), man, overwrite = TRUE)
-  system("rm -rf .Rd2pdf*")
-}
-
-sapply(c("data","design","basics","model","multivariate"), create_manuals)
+# unlink('radiant.data.pdf')
+# setwd("~")
+# unlink('radiant.data.pdf')
+# system("R CMD Rd2pdf gh/radiant.data --no-preview")
+# system("rm -rf .Rd2pdf*")
+# setwd("~/gh/docs/")
+# file.copy("~/radiant.data.pdf","radiant.data.pdf",overwrite = TRUE)
+# system("rm -rf .Rd2pdf*")
+#
+# create_manuals <- function(x) {
+#   app <- paste0("radiant.",x)
+#   man <- paste0(app,".pdf")
+#   unlink(man)
+#   setwd("~")
+#   unlink(man)
+#   system(paste0("R CMD Rd2pdf gh/",app," --no-preview"))
+#   system("rm -rf .Rd2pdf*")
+#   setwd("~/gh/docs/")
+#   file.copy(paste0("~/",man), man, overwrite = TRUE)
+#   system("rm -rf .Rd2pdf*")
+# }
+#
+# sapply(c("data","design","basics","model","multivariate"), create_manuals)
 
 # tinytex::uninstall_tinytex()
